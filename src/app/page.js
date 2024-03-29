a68d593c33d0ef89bd093be52067dbcfcec69227
Abinson Suresh <88340846+Abinsonsuresh@users.noreply.github.com>
2024-03-29 20:35:07 +0530

    layout setup finished

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,16 @@
 'use client'
 import Image from "next/image";
+import { useState } from "react";
 
 export default function Home() {
+  const [items, setItems] = useState([
+    { name: 'Coffee', price: 4.96 },
+    { name: 'movie', price: 3.6 },
+    { name: 'Coffee', price: 4.96 },
+
+  ])
+
+  const [total, setTotal] = useState(0)
   return (
     <div>
       <form action="">
@@ -9,6 +18,25 @@ export default function Home() {
         <input type="number" placeholder="Enter $" />
         <button type="submit">+</button>
       </form>
+      <ul className="flex flex-col">
+        {items.map((item,index) => (
+          <li key={index} className="flex justify-between w-full">
+          <div className="flex p-4 w-full justify-between gap-4">
+          <p>{item.name}</p>
+            <p>{item.price}</p>
+          </div>
+          <button className="ml-8 w-16 p-4 bg-slate-600">X</button>
+          </li>
+        ))}
+      </ul>
+      {
+        items.length < 1 ? ('') : (
+          <div>
+            <span>Total</span>
+            <span>${total}</span>
+          </div>
+        )
+      }
     </div>
   );
 }
