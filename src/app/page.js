4657d963c924f5bad764f3f72e5ebe2a8527fde3
Abinson Suresh <88340846+Abinsonsuresh@users.noreply.github.com>
2024-03-30 00:25:51 +0530

    layout css added

--- src/app/page.js
+++ src/app/page.js
@@ -62,37 +62,40 @@ export default function Home() {
 
 
 
-
-
-
-
   return (
-    <div>
+    <div className="min-h-screen flex justify-start flex-col items-center p-4 ">
+      <div className="my-12">
+        <h1 className="font-bold text-5xl">Expense Tracker</h1>
+        <p className="text-center">(using Next js & firebase)</p>
+      </div>
+    <div className="bg-slate-900 p-4  h-full rounded-sm">
 
-      <form action="">
-        <input value={newItem.name} onChange={(e) => setNewItem({ ...newItem, name: e.target.value })} type="text" placeholder="Enter your Expense" />
-        <input value={newItem.price} onChange={(e) => setNewItem({ ...newItem, price: e.target.value })} type="number" placeholder="Enter $" />
-        <button type="submit" onClick={addItem}>+</button>
+      <form className="grid grid-cols-6" action="">
+        <input className="col-span-3  rounded-sm p-2" value={newItem.name} onChange={(e) => setNewItem({ ...newItem, name: e.target.value })} type="text" placeholder="Enter your Expense" />
+        <input className="col-span-2 rounded-sm p-2 mx-2" value={newItem.price} onChange={(e) => setNewItem({ ...newItem, price: e.target.value })} type="number" placeholder="Enter $" />
+        <button className="bg-green-500 p-4 text-xl" type="submit" onClick={addItem}>+</button>
       </form>
-      <ul className="flex flex-col">
+      <ul className="flex flex-col my-6 overflow-y-scroll scroll-smooth">
         {items.map((item, id) => (
-          <li key={id} className="flex justify-between w-full">
-            <div className="flex p-4 w-full justify-between gap-4">
+          <li key={id} className="flex justify-between my-2 border-b border-[#5454544f] w-full">
+            <div className="flex  p-4 w-full justify-between gap-4">
               <p>{item.name}</p>
-              <p>{item.price}</p>
+              <p>${item.price}</p>
             </div>
-            <button onClick={()=> deleteItem(item.id)} className="ml-8 w-16 p-4 bg-slate-600">X</button>
+            <button onClick={()=> deleteItem(item.id)} className="ml-8 w-16 p-4 bg-red-600">X</button>
           </li>
         ))}
       </ul>
       {
         items.length < 1 ? ('') : (
-          <div>
+          <div className="flex p-4 w-full justify-between gap-4">
             <span>Total</span>
             <span>${total}</span>
           </div>
         )
       }
+    </div>
+
     </div>
   );
 }
